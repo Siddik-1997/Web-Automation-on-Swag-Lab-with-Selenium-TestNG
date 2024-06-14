752469653b087ce055de87fbf116fc85aa511bf5
Siddik-1997 <abubakkar1045@gmail.com>
2024-06-14 21:09:56 +0600

    add few code

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,4 +1,6 @@
 package runner;
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
@@ -11,6 +13,11 @@ public class TestRunner extends Setup {
     public void doLogin(){
         loginPage = new LoginPage(driver);
         loginPage.doLogin("standard_user", "secret_sauce");
+
+        // Assertion
+        String actualResult= driver.findElement(By.className("product_label")).getText();
+        String expectedResult= "Products";
+        Assert.assertEquals(actualResult,expectedResult);
     }
 
 
