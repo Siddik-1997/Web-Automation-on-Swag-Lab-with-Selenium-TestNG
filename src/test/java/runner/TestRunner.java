569b4a9cae42c532dcc0417cd397199a3609be3c
Siddik-1997 <abubakkar1045@gmail.com>
2024-06-15 15:08:24 +0600

    Json file update

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,18 +1,26 @@
 package runner;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ParseException;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.LoginPage;
 import utils.Setup;
+import utils.Utility;
+
+import java.io.IOException;
 
 public class TestRunner extends Setup {
     LoginPage loginPage;
     HomePage homePage;
     @Test(priority = 1)
-    public void doLogin(){
+    public void doLogin() throws IOException, ParseException {
         loginPage = new LoginPage(driver);
-        loginPage.doLogin("standard_user", "secret_sauce");
+        JSONObject userObject = Utility.loadJSONFile("./src/test/resources/User.json");
+        String userName= (String) userObject.get("userName");
+        String password= (String) userObject.get("password");
+        loginPage.doLogin(userName,password);
 
         // Assertion
         String actualResult= driver.findElement(By.className("product_label")).getText();
--- src/test/java/utils/Utility.java
+++ src/test/java/utils/Utility.java
@@ -1,9 +1,15 @@
 package utils;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.Duration;
 
 public class Utility {
@@ -14,5 +20,13 @@ public class Utility {
     public static void waitForElements(WebDriver webDriver, WebElement webElement, int timeunitSeconds){
         WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(timeunitSeconds));
         wait.until(ExpectedConditions.visibilityOf(webElement));
+    }
+
+    public static JSONObject loadJSONFile(String fileLocation) throws IOException, ParseException { // Read json file's data
+        JSONParser jsonParser= new JSONParser();
+        Object obj= jsonParser.parse(new FileReader(fileLocation));
+        JSONObject jsonObject= (JSONObject) obj;
+        return jsonObject;
+
     }
 }
